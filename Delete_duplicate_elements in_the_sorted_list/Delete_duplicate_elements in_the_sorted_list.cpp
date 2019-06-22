47b7c7746bc6aa73bab9a5a4d1c41e846b109c55
QTAnt <zhangcong129@126.com>
2019-06-22 23:48:24 +0800

    Delete_duplicate_elements in_the_sorted_list

---
+++ Delete_duplicate_elements in_the_sorted_list/Delete_duplicate_elements in_the_sorted_list.cpp
@@ -0,0 +1,40 @@
+﻿//Delete_duplicate_elements in_the_sorted_list：删除排序链表中的重复元素
+//https ://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+//示例 1 :
+//输入 : 1->1->2
+//输出 : 1->2
+//示例 2 :
+//输入 : 1->1->2->3->3
+//输出 : 1->2->3
+
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+		if (!head)
+			return head;
+		ListNode* cur = head->next, *pre = head;
+		while (cur)
+		{
+			if (pre->val == cur->val)
+			{
+				pre->next = cur->next;
+				cur = cur->next;
+			}
+			else
+			{
+				pre = cur;
+				cur = cur->next;
+			}
+		}
+		return head;
+	}
+};
