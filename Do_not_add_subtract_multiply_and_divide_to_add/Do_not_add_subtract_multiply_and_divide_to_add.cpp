1a1ba2de00e0d8253f6d31a1932ed62c705e3d5e
QTAnt <zhangcong129@126.com>
2019-07-08 22:00:19 +0800

    Do_not_add_subtract_multiply_and_divide_to_add

---
+++ Do_not_add_subtract_multiply_and_divide_to_add/Do_not_add_subtract_multiply_and_divide_to_add.cpp
@@ -0,0 +1,14 @@
+Do_not_add_subtract_multiply_and_divide_to_add：不用加减乘除做加法
+
+class Solution {
+public:
+int Add(int num1,int num2) {
+		while( num2 != 0 ){
+		int sum = num1 ^ num2; //得到相加后不包含进位的数据
+		int carray = (num1 & num2) << 1; //得到两数相加的进位
+		num1 = sum; //两个结果相加，直到进位为0
+		num2 = carray;
+		} 
+	return num1;
+	}
+};
