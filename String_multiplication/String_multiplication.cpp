9dab5dbd45ed5e33a6b61feb88b292984ee6a0ae
QTAnt <zhangcong129@126.com>
2019-08-16 22:22:29 +0800

    String_multiplication

---
+++ String_multiplication/String_multiplication.cpp
@@ -0,0 +1,44 @@
+﻿//String_multiplication:字符串相乘
+//https ://leetcode-cn.com/problems/multiply-strings/
+//给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+//示例 1 :
+//输入 : num1 = "2", num2 = "3"
+//输出 : "6"
+//示例 2 :
+//输入 : num1 = "123", num2 = "456"
+//输出 : "56088"
+//说明：
+//num1 和 num2 的长度小于110。
+//num1 和 num2 只包含数字 0 - 9。
+//num1 和 num2 均不以零开头，除非是数字 0 本身。
+//不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理
+
+class Solution {
+public:
+	string multiply(string num1, string num2) {
+		if (num1 == "0" || num2 == "0")return "0";
+		string ans = "";
+		int c = 0;
+		int i = num1.size() + num2.size() - 2;
+		int x = 0;
+		while (i >= 0){
+			x = c;
+			//cout<<x<<"."<<endl;
+			for (int j = i<num1.size() ? i : (num1.size() - 1); i - j<num2.size() && j >= 0; j--){
+				x += ((num1[j] - '0')*(num2[i - j] - '0'));
+				//cout<<j<<" "<<i-j;
+				//cout<<"="<<x<<endl;
+				//cout<<num1[j]<<" "<<num2[i-j];
+				//cout<<"="<<x<<endl;
+			}
+			c = x / 10;
+			x = x % 10;
+			char a = '0' + x;
+			ans = a + ans;
+			//cout<<i<<endl;
+			i--;
+		}
+		if (c != 0)ans = char(c + '0') + ans;
+		return ans;
+	}
+};
