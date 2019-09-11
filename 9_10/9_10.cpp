f8a029fd009e5f585d19bdcca6d4b7048e155b4a
QTAnt <zhangcong129@126.com>
2019-09-11 23:59:39 +0800

    baidu

---
+++ 9_10/9_10.cpp
@@ -0,0 +1,23 @@
+////判断整数是奇数还是偶数；分别返回False、True
+//#include <iostream>
+//using namespace std;
+//
+//bool IsOdd(int n)
+//{
+//	if (n % 2 == 0)
+//		return false; 
+//	else
+//		return true;
+//}
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	bool ret = IsOdd(n);
+//	if (ret == true)
+//		cout << "True" << endl;
+//	else
+//		cout << "False" << endl;
+//	return 0;
+//}
+
