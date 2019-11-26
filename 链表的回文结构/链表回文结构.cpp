afe6c9a88d6350ba4fad9513cbc885d1a190eba5
QTAnt <zhangcong129@126.com>
2019-11-26 18:34:29 +0800

    链表的回文结构

--- 链表的回文结构/链表回文结构.cpp
+++ 链表的回文结构/链表回文结构.cpp
@@ -1,4 +1,4 @@
-链表是逆置结构：
+链表的回文结构：
 https://www.nowcoder.com/practice/d281619e4b3e4a60a2cc66ea32855bfa?tpId=49&&tqId=29370&rp=1&ru=/activity/oj&qru=/ta/2016test/question-ranking
 对于一个链表，请设计一个时间复杂度为O(n),额外空间复杂度为O(1)的算法，判断其是否为回文结构。
 给定一个链表的头指针A，请返回一个bool值，代表其是否为回文结构。
